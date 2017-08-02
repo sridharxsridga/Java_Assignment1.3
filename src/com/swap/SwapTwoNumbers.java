d2f6eb0910e9fee34e1c2d5a226eae40f12773d2
sridharxsridga <sridhar.sridhar.sridhar404@gmail.com>
2017-08-02 16:14:18 +0530

    First Commit
    Change-Id: Iaeb9477c084165b062e3218b922e03c6790fd8f7
    Signed-off-by: sridharxsridga <sridhar.sridhar.sridhar404@gmail.com>

---
+++ src/com/swap/SwapTwoNumbers.java
@@ -0,0 +1,22 @@
+/* Write a program that swaps the values of two variables without using third variable.
+ * 
+ * Expected Output: Print the swapped values of both the variables.
+ */
+
+package com.swap;
+
+public class SwapTwoNumbers {
+
+	public static void main(String[] args) {
+		int firstVariable = -10;
+		int secondVariable = 20;
+		System.out.println("Before Swapping  Value for firstVariable is " + firstVariable
+				+ " and for secondVariable is " + secondVariable);
+		firstVariable = firstVariable + secondVariable;
+		secondVariable = firstVariable - secondVariable;
+		firstVariable = firstVariable - secondVariable;
+		System.out.println("Before Swapping  Value for firstVariable is " + firstVariable
+				+ " and for secondVariable is " + secondVariable);
+	}
+
+}
